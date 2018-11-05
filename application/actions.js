5b092abf8791348d69d532c419796adb0a38b5b1
Ryan Jones <ryan@system88.com>
2018-11-04 19:13:01 -0700

    adding in graphql query

---
+++ application/actions.js
@@ -0,0 +1,38 @@
+import { API, graphqlOperation } from 'aws-amplify';
+
+// Action Types
+
+export const FETCH_SERVICES_COMPLETE              = 'FETCH_SERVICES_COMPLETE';
+export const FETCH_MORE_SERVICES_COMPLETE         = 'FETCH_MORE_SERVICES_COMPLETE';
+
+// GraphQL Queries
+
+const FetchServices = `query EventConnection($nextToken: String) {
+    listServices(nextToken: $nextToken) {
+      services {
+        id
+        name
+        language
+        description
+      }
+      nextToken
+    }
+  }`;
+
+
+// Exported Actions
+
+export function fetchServices(nextToken) {
+    return dispatch => {
+      API.graphql(graphqlOperation(FetchServices, { nextToken }))
+        .then(response => {
+          const data = response.data;
+  
+          dispatch({
+            type: nextToken ? FETCH_MORE_SERVICES_COMPLETE : FETCH_SERVICES_COMPLETE,
+            events: data.listEvents.items,
+            nextToken: data.listEvents.nextToken
+          });
+        });
+    };
+  }
